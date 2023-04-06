a61126633ce7aa5e90704f8b7984191dc9457dc3
sathmika <kulunu.20210786@iit.ac.lk>
2023-04-06 09:39:29 +0530

    do css corrections on about us page

--- zoodoo/client/src/pages/AboutUs.js
+++ zoodoo/client/src/pages/AboutUs.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import './about_us.css';
-import St1 from '../images/student1.jpg';
-import St2 from '../images/student2.jpg';
-import St3 from '../images/student3.jpg';
-import St4 from '../images/student4.jpg';
-import St5 from '../images/student5.jpg';
+import React from "react";
+import "./about_us.css";
+import St1 from "../images/student1.jpg";
+import St2 from "../images/student2.jpg";
+import St3 from "../images/student3.jpg";
+import St4 from "../images/student4.jpg";
+import St5 from "../images/student5.jpg";
 
 function AboutUs() {
   return (
     <div className="icon" align="center">
       <h1 id="studentHeading">About Us</h1>
-      <p>This project is regarding the implementation and design of Zoodoo,an advanced food ordering application. The goal of the Zoodoo project is to combine safer and healthier eating with food ordering applications in order to inform people about the health risks associated with various food types and to offer healthier substitutes so that they can live as healthy a life as possible.</p>
+      <p>
+        This project is regarding the implementation and design of Zoodoo,an
+        advanced food ordering application. The goal of the Zoodoo project is to
+        combine safer and healthier eating with food ordering applications in
+        order to inform people about the health risks associated with various
+        food types and to offer healthier substitutes so that they can live as
+        healthy a life as possible.
+      </p>
       <h2>Our Team</h2>
       <div id="students">
         <div className="studentBox">
